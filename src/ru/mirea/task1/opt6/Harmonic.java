8ebde71b99ab2ef2ef27f4f599640d39b5f296e9
xenybyte <xenybyte@gmail.com>
2022-12-06 14:31:05 +0000

    1.6

--- src/ru/mirea/task1/opt6/Harmonic.java
+++ src/ru/mirea/task1/opt6/Harmonic.java
@@ -2,7 +2,7 @@ package ru.mirea.task1.opt6;
 
 public class Harmonic {
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i < 11; i++) {
             System.out.println(1.0 / i);
         }
     }
